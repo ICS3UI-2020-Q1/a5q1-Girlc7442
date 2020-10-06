0a23c525102cea37d2d4f97b4ca5771567b06054
Girlc7442 <girlc7442@wrdsb.ca>
2020-10-06 12:59:19 +0000

    Finished with comments

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
  *
- * @author 
+ * @author Cole Girling
  */
 public class Main {
 
@@ -9,7 +11,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user for an integer they would like to create a multiplication table for
+    System.out.println("Please enter an integer to create a multiplication table for");
+    int usernumber = input.nextInt();
+    
+    //while i is less than 13 this will run
+     for(int i = 1; i <= 12; i++){
+       //loop action: multiply i and the userNumber
+       int times = i * usernumber;
+       //loop action: display the times of i and userNumber
+       System.out.println(i + " x " + usernumber + " = " + times);
+     }
     
   }
 }
